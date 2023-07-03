cf3480a3298fe34ab2456e219d92dbef4bf9b0f9
Jimmy Clairy <jimcl@hotmail.fr>
2023-07-03 16:21:14 +0200

    Litle modif

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
 	function handleBlur() {
 		if (!inputValue.includes('@')) {
-			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+			alert("Attention, il n'y a pas d'@', ceci n'est pas une adresse valide 😥")
 		}
 	}
 
